b7133055a57ef706422c32c62008319b6a9ae27f
NOTE\Alexandre <alexalsil11@gmail.com>
2017-07-24 14:06:47 -0300

    WebService

---
+++ mainWebservice/java/patrocinador/model/domain/Patrocinador.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patrocinador.model.domain;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class Patrocinador {
+     private int idPatrocinador;
+
+    private String nomePatrocinador;
+
+    public int getIdPatrocinador() {
+        return idPatrocinador;
+    }
+
+    public void setIdPatrocinador(int idPatrocinador) {
+        this.idPatrocinador = idPatrocinador;
+    }
+
+    public String getNomePatrocinador() {
+        return nomePatrocinador;
+    }
+
+    public void setNomePatrocinador(String nomePatrocinador) {
+        this.nomePatrocinador = nomePatrocinador;
+    }
+    
+    
+}
